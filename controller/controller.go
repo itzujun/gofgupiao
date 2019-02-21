f75c86da87c8be8f198a00d0fe1bba3a6c2dfde4
cage <953761580@qq.com>
2019-02-21 17:58:56 +0800

    111

--- controller/controller.go
+++ controller/controller.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"github.com/itzujun/gofgupiao/analyzer"
 	"github.com/itzujun/gofgupiao/basic"
 	"github.com/itzujun/gofgupiao/downloader"
 	"github.com/itzujun/gofgupiao/middleware"
@@ -9,9 +10,26 @@ import (
 
 var wg sync.WaitGroup
 
-var logger basic.ConsoleLogger
+var logger = basic.NewSimpleLogger() // 日志记录器
 
 type Controller struct {
+	StartUrl   string                //初始爬行Url
 	Downloader downloader.Downloader // 下载器
-	Channel    *middleware.Channel	//
+	Channel    *middleware.Channel   //管道
+	WorkPool   *middleware.WorkPool  //工作池
+	Parser     analyzer.Analyzer     //解析页函数
+}
+
+func NewController(startUrl string, downloader downloader.Downloader,
+	channel *middleware.Channel, workPool *middleware.WorkPool, parser analyzer.Analyzer) *Controller {
+	return &Controller{StartUrl: startUrl, Downloader: downloader,
+		Channel: channel, WorkPool: workPool, Parser: parser}
+}
+
+func (ctrl *Controller) Go() {
+	basic.Config.StartUrl = ctrl.StartUrl
+	basic.InitConfig()
+	ctrl.Downloader = downloader.NewDownloader()
+
+
 }
--- downloader/downloader.go
+++ downloader/downloader.go
@@ -1,13 +1,25 @@
 package downloader
 
+import (
+	"github.com/itzujun/gofgupiao/basic"
+	"net/http"
+)
+
 type Downloader struct {
-	//
+	client *http.Client
 }
 
-func NewDownloader() {
-
+func NewDownloader() *Downloader {
+	return &Downloader{}
 }
 
-func (this *Downloader) Download() {
-	//
+func (this *Downloader) Download(req *basic.Request) *basic.Response {
+	for k, v := range basic.Config.HttpHead {
+		req.GetReq().Header.Set(k, v)
+	}
+	httpRes, err := this.client.Do(req.GetReq())
+	if err != nil {
+		return nil
+	}
+	return basic.NewResponse(httpRes, req.GetIndex())
 }
