36f072475a91704244f40dacf3e479646b400833
cage <953761580@qq.com>
2019-02-22 11:08:31 +0800

    111

--- analyzer/analyzer.go
+++ analyzer/analyzer.go
@@ -11,22 +11,22 @@ import (
 	"strings"
 )
 
-type Shares struct {
-	Name    string
-	Code    string
-	Url     string
-	ApiCode string
-}
-
-type SharesRes struct {
-	Name     string
-	Code     string
-	Open     string
-	High     string
-	Close    string
-	Volume   string
-	PreClose string
-}
+//type Shares struct {
+//	Name    string
+//	Code    string
+//	Url     string
+//	ApiCode string
+//}
+//
+//type SharesRes struct {
+//	Name     string
+//	Code     string
+//	Open     string
+//	High     string
+//	Close    string
+//	Volume   string
+//	PreClose string
+//}
 
 type GenAnalyzer interface {
 	AnalyzeHtml(httpRes *http.Response) []Shares
